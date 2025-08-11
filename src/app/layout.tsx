8998e0adf86df864fa715bd91ca0d381540b2115
Tiago <tiagolpadua@gmail.com>
2025-08-11 16:04:19 -0300

    migracao next

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'React App',
+  description: 'Web site created with Next.js.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    return <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+}
